e5e8fb20c4a47ec629bb2571cbc487732f284a37
Febrian Rendak <febriananugrah92@gmail.com>
2023-01-31 14:26:21 +0700

    multiple read input

--- app/server.go
+++ app/server.go
@@ -1,36 +1,66 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+    "fmt"
+    "io"
+    "net"
+    "os"
+    // Uncomment this block to pass the first stage
+    // "net"
+    // "os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    buffSize := 1024
+    totalThread := 5
+    fmt.Println("Logs from your program will appear here!")
+    l, err := net.Listen("tcp", "0.0.0.0:6379")
+    if err != nil {
+        fmt.Println("Failed to bind to port 6379")
+        os.Exit(1)
+    }
+    defer l.Close()
 
-	// Uncomment this block to pass the first stage
+    stopChan := make(chan bool)
+    for i := 0; i < totalThread; i++ {
+        go func(tNo int) {
+            for {
+                fmt.Printf("[WORKER %d] waiting connection\n", tNo)
+                conn, err := l.Accept()
+                if err != nil {
+                    fmt.Println("Error accepting connection: ", err.Error())
+                    os.Exit(1)
+                }
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	defer l.Close()
+                fmt.Printf("[WORKER %d] accepting connection\n", tNo)
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
+                for {
+                    buff := make([]byte, buffSize)
+                    n, err := conn.Read(buff)
+                    if err == io.EOF {
+                        break
+                    }
+                    if err != nil {
+                        fmt.Printf("error when reading client request, err: %s\n", err)
+                    }
 
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Printf("error: %s", err)
-	}
+                    if n > buffSize {
+                        fmt.Println("got buffer bigger than expected")
+                    }
+
+                    fmt.Printf("[WORKER %d] reading request, got: %#v\n", tNo, string(buff[:n]))
+                    _, err = conn.Write([]byte("+PONG\r\n"))
+                    if err != nil {
+                        fmt.Printf("error: %s", err)
+                    }
+                }
+
+                fmt.Printf("[WORKER %d] closing connection\n", tNo)
+                conn.Close()
+            }
+        }(i)
+    }
+
+    <-stopChan
 }
