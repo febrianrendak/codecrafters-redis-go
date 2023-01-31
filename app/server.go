9c8235497c50e0663da93d35b4586661b74f4068
Febrian Rendak <febriananugrah92@gmail.com>
2023-01-31 11:03:05 +0700

    response any incoming connection with PONG

--- app/server.go
+++ app/server.go
@@ -1,29 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
-
-    // Uncomment this block to pass the first stage
-    // "net"
-    // "os"
+	"fmt"
+	"net"
+	"os"
+	// Uncomment this block to pass the first stage
+	// "net"
+	// "os"
 )
 
 func main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    fmt.Println("Logs from your program will appear here!")
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
 
-    // Uncomment this block to pass the first stage
+	// Uncomment this block to pass the first stage
 
-    l, err := net.Listen("tcp", "0.0.0.0:6379")
-    if err != nil {
-        fmt.Println("Failed to bind to port 6379")
-        os.Exit(1)
-    }
-    _, err = l.Accept()
-    if err != nil {
-        fmt.Println("Error accepting connection: ", err.Error())
-        os.Exit(1)
-    }
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Printf("error: %s", err)
+	}
 }
